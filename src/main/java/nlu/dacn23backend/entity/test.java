a5f18bab1f2f2b455b82ef7754ffa4b8eb97c263
Thành Nguyễn Trí <91453590+thahn0402@users.noreply.github.com>
2023-07-24 21:31:17 +0700

    update

---
+++ src/main/java/nlu/dacn23backend/entity/test.java
@@ -0,0 +1,162 @@
+/*
+
+1. Hài kịch
+
+Hài kịch là một thể loại của kịch, hướng vào sợ cười nhạo cái xấu xa, lố bịch, lạc hậu,… đối lập với các chuẩn mực về cái tốt đẹp, tiến bộ. Trong hài kịch có nhiều hình thức xung đột, nhưng phổ biến nhất là sự không tương xứng giữa cái bên trong với cái bên ngoài. Xung đột kịch biểu hiện qua hành động kịch với các sự việc, tình huống gây cười. Nhân vật chính trong hài kịch là những kiểu người có tính cách tiêu biểu cho các thói xấu đáng phê phán: hà tiện, tham lam, kiêu căng, khoe mẽ,… Lời đối thoại trong hài kịch mang đậm tính khẩu ngữ; cấu trúc đối thoại dựa trên những nội dung đối nghịch. Hài kịch thường sử dụng các thủ pháp trào phúng như: tạo tình huống kịch tính, cải trang; dùng điệu bộ gây cười; dùng thủ pháp phóng đại, chơi chữ, gây hiểu lầm, thoại bỏ lửng, nhại,…
+
+2. Truyện cười
+
+- Truyện cười là một thể loại tự sự có dung lượng nhỏ, dùng tiếng cười nhằm chế giễu những thói hư tật xấu, những điều trái tự nhiên, trái thuần phong mỹ tục của con người còn nhằm mục đích giải trí. Truyện cười thường ngắn, cốt truyện tập trung vào sự việc có yếu tố gây cười, tình huống trớ trêu, những nghịch lí trong đời sống,… Bối cảnh được xây dựng trong truyện cười thường bị cường điệu so với thực tế, có yếu tố bất ngờ. Nhân vật chính trong truyện cười thường là đối tượng bị chế giễu. Ngôn ngữ truyện cười dân dã, nhiều ẩn ý.
+
+- Truyện cười thường là truyện dân gian, tuy nhiên cũng có cả ở hình thức truyện kể của văn học viết.
+
+3. Câu hỏi tu từ
+
+Câu hỏi tu từ là câu hỏi không dùng để hỏi mà để khẳng định, phủ định, bộc lộ cảm xúc,… Khác với câu hỏi thông thường, câu hỏi tu từ được sử dụng nhằm đạt một số hiệu quả giao tiếp như tăng sắc thái biểu cảm, biểu đạt ý nghĩa một cách tế nhị, uyển chuyển.
+
+4. Nghĩa tường minh và nghĩa hàm ẩn của câu
+
+Nghĩa tường minh là nghĩa được diễn đạt trực tiếp bằng các từ ngữ trong câu. Nghĩa hàm ẩn là nghĩa được suy ra từ nghĩa tường minh của cả câu, từ nghĩa của từ ngữ trong câu và từ ngữ cảnh sử dụng câu.
+
+Xem thêm các bài Soạn văn lớp 8 Kết nối tri thức hay nhất, ngắn gọn khác:
+
+1. Hài kịch
+
+Hài kịch là một thể loại của kịch, hướng vào sợ cười nhạo cái xấu xa, lố bịch, lạc hậu,… đối lập với các chuẩn mực về cái tốt đẹp, tiến bộ. Trong hài kịch có nhiều hình thức xung đột, nhưng phổ biến nhất là sự không tương xứng giữa cái bên trong với cái bên ngoài. Xung đột kịch biểu hiện qua hành động kịch với các sự việc, tình huống gây cười. Nhân vật chính trong hài kịch là những kiểu người có tính cách tiêu biểu cho các thói xấu đáng phê phán: hà tiện, tham lam, kiêu căng, khoe mẽ,… Lời đối thoại trong hài kịch mang đậm tính khẩu ngữ; cấu trúc đối thoại dựa trên những nội dung đối nghịch. Hài kịch thường sử dụng các thủ pháp trào phúng như: tạo tình huống kịch tính, cải trang; dùng điệu bộ gây cười; dùng thủ pháp phóng đại, chơi chữ, gây hiểu lầm, thoại bỏ lửng, nhại,…
+
+2. Truyện cười
+
+- Truyện cười là một thể loại tự sự có dung lượng nhỏ, dùng tiếng cười nhằm chế giễu những thói hư tật xấu, những điều trái tự nhiên, trái thuần phong mỹ tục của con người còn nhằm mục đích giải trí. Truyện cười thường ngắn, cốt truyện tập trung vào sự việc có yếu tố gây cười, tình huống trớ trêu, những nghịch lí trong đời sống,… Bối cảnh được xây dựng trong truyện cười thường bị cường điệu so với thực tế, có yếu tố bất ngờ. Nhân vật chính trong truyện cười thường là đối tượng bị chế giễu. Ngôn ngữ truyện cười dân dã, nhiều ẩn ý.
+
+- Truyện cười thường là truyện dân gian, tuy nhiên cũng có cả ở hình thức truyện kể của văn học viết.
+
+3. Câu hỏi tu từ
+
+Câu hỏi tu từ là câu hỏi không dùng để hỏi mà để khẳng định, phủ định, bộc lộ cảm xúc,… Khác với câu hỏi thông thường, câu hỏi tu từ được sử dụng nhằm đạt một số hiệu quả giao tiếp như tăng sắc thái biểu cảm, biểu đạt ý nghĩa một cách tế nhị, uyển chuyển.
+
+4. Nghĩa tường minh và nghĩa hàm ẩn của câu
+
+Nghĩa tường minh là nghĩa được diễn đạt trực tiếp bằng các từ ngữ trong câu. Nghĩa hàm ẩn là nghĩa được suy ra từ nghĩa tường minh của cả câu, từ nghĩa của từ ngữ trong câu và từ ngữ cảnh sử dụng câu.
+
+Xem thêm các bài Soạn văn lớp 8 Kết nối tri thức hay nhất, ngắn gọn khác:
+
+1. Hài kịch
+
+Hài kịch là một thể loại của kịch, hướng vào sợ cười nhạo cái xấu xa, lố bịch, lạc hậu,… đối lập với các chuẩn mực về cái tốt đẹp, tiến bộ. Trong hài kịch có nhiều hình thức xung đột, nhưng phổ biến nhất là sự không tương xứng giữa cái bên trong với cái bên ngoài. Xung đột kịch biểu hiện qua hành động kịch với các sự việc, tình huống gây cười. Nhân vật chính trong hài kịch là những kiểu người có tính cách tiêu biểu cho các thói xấu đáng phê phán: hà tiện, tham lam, kiêu căng, khoe mẽ,… Lời đối thoại trong hài kịch mang đậm tính khẩu ngữ; cấu trúc đối thoại dựa trên những nội dung đối nghịch. Hài kịch thường sử dụng các thủ pháp trào phúng như: tạo tình huống kịch tính, cải trang; dùng điệu bộ gây cười; dùng thủ pháp phóng đại, chơi chữ, gây hiểu lầm, thoại bỏ lửng, nhại,…
+
+2. Truyện cười
+
+- Truyện cười là một thể loại tự sự có dung lượng nhỏ, dùng tiếng cười nhằm chế giễu những thói hư tật xấu, những điều trái tự nhiên, trái thuần phong mỹ tục của con người còn nhằm mục đích giải trí. Truyện cười thường ngắn, cốt truyện tập trung vào sự việc có yếu tố gây cười, tình huống trớ trêu, những nghịch lí trong đời sống,… Bối cảnh được xây dựng trong truyện cười thường bị cường điệu so với thực tế, có yếu tố bất ngờ. Nhân vật chính trong truyện cười thường là đối tượng bị chế giễu. Ngôn ngữ truyện cười dân dã, nhiều ẩn ý.
+
+- Truyện cười thường là truyện dân gian, tuy nhiên cũng có cả ở hình thức truyện kể của văn học viết.
+
+3. Câu hỏi tu từ
+
+Câu hỏi tu từ là câu hỏi không dùng để hỏi mà để khẳng định, phủ định, bộc lộ cảm xúc,… Khác với câu hỏi thông thường, câu hỏi tu từ được sử dụng nhằm đạt một số hiệu quả giao tiếp như tăng sắc thái biểu cảm, biểu đạt ý nghĩa một cách tế nhị, uyển chuyển.
+
+4. Nghĩa tường minh và nghĩa hàm ẩn của câu
+
+Nghĩa tường minh là nghĩa được diễn đạt trực tiếp bằng các từ ngữ trong câu. Nghĩa hàm ẩn là nghĩa được suy ra từ nghĩa tường minh của cả câu, từ nghĩa của từ ngữ trong câu và từ ngữ cảnh sử dụng câu.
+
+Xem thêm các bài Soạn văn lớp 8 Kết nối tri thức hay nhất, ngắn gọn khác:
+
+1. Hài kịch
+
+Hài kịch là một thể loại của kịch, hướng vào sợ cười nhạo cái xấu xa, lố bịch, lạc hậu,… đối lập với các chuẩn mực về cái tốt đẹp, tiến bộ. Trong hài kịch có nhiều hình thức xung đột, nhưng phổ biến nhất là sự không tương xứng giữa cái bên trong với cái bên ngoài. Xung đột kịch biểu hiện qua hành động kịch với các sự việc, tình huống gây cười. Nhân vật chính trong hài kịch là những kiểu người có tính cách tiêu biểu cho các thói xấu đáng phê phán: hà tiện, tham lam, kiêu căng, khoe mẽ,… Lời đối thoại trong hài kịch mang đậm tính khẩu ngữ; cấu trúc đối thoại dựa trên những nội dung đối nghịch. Hài kịch thường sử dụng các thủ pháp trào phúng như: tạo tình huống kịch tính, cải trang; dùng điệu bộ gây cười; dùng thủ pháp phóng đại, chơi chữ, gây hiểu lầm, thoại bỏ lửng, nhại,…
+
+2. Truyện cười
+
+- Truyện cười là một thể loại tự sự có dung lượng nhỏ, dùng tiếng cười nhằm chế giễu những thói hư tật xấu, những điều trái tự nhiên, trái thuần phong mỹ tục của con người còn nhằm mục đích giải trí. Truyện cười thường ngắn, cốt truyện tập trung vào sự việc có yếu tố gây cười, tình huống trớ trêu, những nghịch lí trong đời sống,… Bối cảnh được xây dựng trong truyện cười thường bị cường điệu so với thực tế, có yếu tố bất ngờ. Nhân vật chính trong truyện cười thường là đối tượng bị chế giễu. Ngôn ngữ truyện cười dân dã, nhiều ẩn ý.
+
+- Truyện cười thường là truyện dân gian, tuy nhiên cũng có cả ở hình thức truyện kể của văn học viết.
+
+3. Câu hỏi tu từ
+
+Câu hỏi tu từ là câu hỏi không dùng để hỏi mà để khẳng định, phủ định, bộc lộ cảm xúc,… Khác với câu hỏi thông thường, câu hỏi tu từ được sử dụng nhằm đạt một số hiệu quả giao tiếp như tăng sắc thái biểu cảm, biểu đạt ý nghĩa một cách tế nhị, uyển chuyển.
+
+4. Nghĩa tường minh và nghĩa hàm ẩn của câu
+
+Nghĩa tường minh là nghĩa được diễn đạt trực tiếp bằng các từ ngữ trong câu. Nghĩa hàm ẩn là nghĩa được suy ra từ nghĩa tường minh của cả câu, từ nghĩa của từ ngữ trong câu và từ ngữ cảnh sử dụng câu.
+
+Xem thêm các bài Soạn văn lớp 8 Kết nối tri thức hay nhất, ngắn gọn khác:
+
+1. Hài kịch
+
+Hài kịch là một thể loại của kịch, hướng vào sợ cười nhạo cái xấu xa, lố bịch, lạc hậu,… đối lập với các chuẩn mực về cái tốt đẹp, tiến bộ. Trong hài kịch có nhiều hình thức xung đột, nhưng phổ biến nhất là sự không tương xứng giữa cái bên trong với cái bên ngoài. Xung đột kịch biểu hiện qua hành động kịch với các sự việc, tình huống gây cười. Nhân vật chính trong hài kịch là những kiểu người có tính cách tiêu biểu cho các thói xấu đáng phê phán: hà tiện, tham lam, kiêu căng, khoe mẽ,… Lời đối thoại trong hài kịch mang đậm tính khẩu ngữ; cấu trúc đối thoại dựa trên những nội dung đối nghịch. Hài kịch thường sử dụng các thủ pháp trào phúng như: tạo tình huống kịch tính, cải trang; dùng điệu bộ gây cười; dùng thủ pháp phóng đại, chơi chữ, gây hiểu lầm, thoại bỏ lửng, nhại,…
+
+2. Truyện cười
+
+- Truyện cười là một thể loại tự sự có dung lượng nhỏ, dùng tiếng cười nhằm chế giễu những thói hư tật xấu, những điều trái tự nhiên, trái thuần phong mỹ tục của con người còn nhằm mục đích giải trí. Truyện cười thường ngắn, cốt truyện tập trung vào sự việc có yếu tố gây cười, tình huống trớ trêu, những nghịch lí trong đời sống,… Bối cảnh được xây dựng trong truyện cười thường bị cường điệu so với thực tế, có yếu tố bất ngờ. Nhân vật chính trong truyện cười thường là đối tượng bị chế giễu. Ngôn ngữ truyện cười dân dã, nhiều ẩn ý.
+
+- Truyện cười thường là truyện dân gian, tuy nhiên cũng có cả ở hình thức truyện kể của văn học viết.
+
+3. Câu hỏi tu từ
+
+Câu hỏi tu từ là câu hỏi không dùng để hỏi mà để khẳng định, phủ định, bộc lộ cảm xúc,… Khác với câu hỏi thông thường, câu hỏi tu từ được sử dụng nhằm đạt một số hiệu quả giao tiếp như tăng sắc thái biểu cảm, biểu đạt ý nghĩa một cách tế nhị, uyển chuyển.
+
+4. Nghĩa tường minh và nghĩa hàm ẩn của câu
+
+Nghĩa tường minh là nghĩa được diễn đạt trực tiếp bằng các từ ngữ trong câu. Nghĩa hàm ẩn là nghĩa được suy ra từ nghĩa tường minh của cả câu, từ nghĩa của từ ngữ trong câu và từ ngữ cảnh sử dụng câu.
+
+Xem thêm các bài Soạn văn lớp 8 Kết nối tri thức hay nhất, ngắn gọn khác:
+
+1. Hài kịch
+
+Hài kịch là một thể loại của kịch, hướng vào sợ cười nhạo cái xấu xa, lố bịch, lạc hậu,… đối lập với các chuẩn mực về cái tốt đẹp, tiến bộ. Trong hài kịch có nhiều hình thức xung đột, nhưng phổ biến nhất là sự không tương xứng giữa cái bên trong với cái bên ngoài. Xung đột kịch biểu hiện qua hành động kịch với các sự việc, tình huống gây cười. Nhân vật chính trong hài kịch là những kiểu người có tính cách tiêu biểu cho các thói xấu đáng phê phán: hà tiện, tham lam, kiêu căng, khoe mẽ,… Lời đối thoại trong hài kịch mang đậm tính khẩu ngữ; cấu trúc đối thoại dựa trên những nội dung đối nghịch. Hài kịch thường sử dụng các thủ pháp trào phúng như: tạo tình huống kịch tính, cải trang; dùng điệu bộ gây cười; dùng thủ pháp phóng đại, chơi chữ, gây hiểu lầm, thoại bỏ lửng, nhại,…
+
+2. Truyện cười
+
+- Truyện cười là một thể loại tự sự có dung lượng nhỏ, dùng tiếng cười nhằm chế giễu những thói hư tật xấu, những điều trái tự nhiên, trái thuần phong mỹ tục của con người còn nhằm mục đích giải trí. Truyện cười thường ngắn, cốt truyện tập trung vào sự việc có yếu tố gây cười, tình huống trớ trêu, những nghịch lí trong đời sống,… Bối cảnh được xây dựng trong truyện cười thường bị cường điệu so với thực tế, có yếu tố bất ngờ. Nhân vật chính trong truyện cười thường là đối tượng bị chế giễu. Ngôn ngữ truyện cười dân dã, nhiều ẩn ý.
+
+- Truyện cười thường là truyện dân gian, tuy nhiên cũng có cả ở hình thức truyện kể của văn học viết.
+
+3. Câu hỏi tu từ
+
+Câu hỏi tu từ là câu hỏi không dùng để hỏi mà để khẳng định, phủ định, bộc lộ cảm xúc,… Khác với câu hỏi thông thường, câu hỏi tu từ được sử dụng nhằm đạt một số hiệu quả giao tiếp như tăng sắc thái biểu cảm, biểu đạt ý nghĩa một cách tế nhị, uyển chuyển.
+
+4. Nghĩa tường minh và nghĩa hàm ẩn của câu
+
+Nghĩa tường minh là nghĩa được diễn đạt trực tiếp bằng các từ ngữ trong câu. Nghĩa hàm ẩn là nghĩa được suy ra từ nghĩa tường minh của cả câu, từ nghĩa của từ ngữ trong câu và từ ngữ cảnh sử dụng câu.
+
+Xem thêm các bài Soạn văn lớp 8 Kết nối tri thức hay nhất, ngắn gọn khác:
+
+1. Hài kịch
+
+Hài kịch là một thể loại của kịch, hướng vào sợ cười nhạo cái xấu xa, lố bịch, lạc hậu,… đối lập với các chuẩn mực về cái tốt đẹp, tiến bộ. Trong hài kịch có nhiều hình thức xung đột, nhưng phổ biến nhất là sự không tương xứng giữa cái bên trong với cái bên ngoài. Xung đột kịch biểu hiện qua hành động kịch với các sự việc, tình huống gây cười. Nhân vật chính trong hài kịch là những kiểu người có tính cách tiêu biểu cho các thói xấu đáng phê phán: hà tiện, tham lam, kiêu căng, khoe mẽ,… Lời đối thoại trong hài kịch mang đậm tính khẩu ngữ; cấu trúc đối thoại dựa trên những nội dung đối nghịch. Hài kịch thường sử dụng các thủ pháp trào phúng như: tạo tình huống kịch tính, cải trang; dùng điệu bộ gây cười; dùng thủ pháp phóng đại, chơi chữ, gây hiểu lầm, thoại bỏ lửng, nhại,…
+
+2. Truyện cười
+
+- Truyện cười là một thể loại tự sự có dung lượng nhỏ, dùng tiếng cười nhằm chế giễu những thói hư tật xấu, những điều trái tự nhiên, trái thuần phong mỹ tục của con người còn nhằm mục đích giải trí. Truyện cười thường ngắn, cốt truyện tập trung vào sự việc có yếu tố gây cười, tình huống trớ trêu, những nghịch lí trong đời sống,… Bối cảnh được xây dựng trong truyện cười thường bị cường điệu so với thực tế, có yếu tố bất ngờ. Nhân vật chính trong truyện cười thường là đối tượng bị chế giễu. Ngôn ngữ truyện cười dân dã, nhiều ẩn ý.
+
+- Truyện cười thường là truyện dân gian, tuy nhiên cũng có cả ở hình thức truyện kể của văn học viết.
+
+3. Câu hỏi tu từ
+
+Câu hỏi tu từ là câu hỏi không dùng để hỏi mà để khẳng định, phủ định, bộc lộ cảm xúc,… Khác với câu hỏi thông thường, câu hỏi tu từ được sử dụng nhằm đạt một số hiệu quả giao tiếp như tăng sắc thái biểu cảm, biểu đạt ý nghĩa một cách tế nhị, uyển chuyển.
+
+4. Nghĩa tường minh và nghĩa hàm ẩn của câu
+
+Nghĩa tường minh là nghĩa được diễn đạt trực tiếp bằng các từ ngữ trong câu. Nghĩa hàm ẩn là nghĩa được suy ra từ nghĩa tường minh của cả câu, từ nghĩa của từ ngữ trong câu và từ ngữ cảnh sử dụng câu.
+
+Xem thêm các bài Soạn văn lớp 8 Kết nối tri thức hay nhất, ngắn gọn khác:
+
+1. Hài kịch
+
+Hài kịch là một thể loại của kịch, hướng vào sợ cười nhạo cái xấu xa, lố bịch, lạc hậu,… đối lập với các chuẩn mực về cái tốt đẹp, tiến bộ. Trong hài kịch có nhiều hình thức xung đột, nhưng phổ biến nhất là sự không tương xứng giữa cái bên trong với cái bên ngoài. Xung đột kịch biểu hiện qua hành động kịch với các sự việc, tình huống gây cười. Nhân vật chính trong hài kịch là những kiểu người có tính cách tiêu biểu cho các thói xấu đáng phê phán: hà tiện, tham lam, kiêu căng, khoe mẽ,… Lời đối thoại trong hài kịch mang đậm tính khẩu ngữ; cấu trúc đối thoại dựa trên những nội dung đối nghịch. Hài kịch thường sử dụng các thủ pháp trào phúng như: tạo tình huống kịch tính, cải trang; dùng điệu bộ gây cười; dùng thủ pháp phóng đại, chơi chữ, gây hiểu lầm, thoại bỏ lửng, nhại,…
+
+2. Truyện cười
+
+- Truyện cười là một thể loại tự sự có dung lượng nhỏ, dùng tiếng cười nhằm chế giễu những thói hư tật xấu, những điều trái tự nhiên, trái thuần phong mỹ tục của con người còn nhằm mục đích giải trí. Truyện cười thường ngắn, cốt truyện tập trung vào sự việc có yếu tố gây cười, tình huống trớ trêu, những nghịch lí trong đời sống,… Bối cảnh được xây dựng trong truyện cười thường bị cường điệu so với thực tế, có yếu tố bất ngờ. Nhân vật chính trong truyện cười thường là đối tượng bị chế giễu. Ngôn ngữ truyện cười dân dã, nhiều ẩn ý.
+
+- Truyện cười thường là truyện dân gian, tuy nhiên cũng có cả ở hình thức truyện kể của văn học viết.
+
+3. Câu hỏi tu từ
+
+Câu hỏi tu từ là câu hỏi không dùng để hỏi mà để khẳng định, phủ định, bộc lộ cảm xúc,… Khác với câu hỏi thông thường, câu hỏi tu từ được sử dụng nhằm đạt một số hiệu quả giao tiếp như tăng sắc thái biểu cảm, biểu đạt ý nghĩa một cách tế nhị, uyển chuyển.
+
+4. Nghĩa tường minh và nghĩa hàm ẩn của câu
+
+Nghĩa tường minh là nghĩa được diễn đạt trực tiếp bằng các từ ngữ trong câu. Nghĩa hàm ẩn là nghĩa được suy ra từ nghĩa tường minh của cả câu, từ nghĩa của từ ngữ trong câu và từ ngữ cảnh sử dụng câu.
+
+Xem thêm các bài Soạn văn lớp 8 Kết nối tri thức hay nhất, ngắn gọn khác:
+*/
