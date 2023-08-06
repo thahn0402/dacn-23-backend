60270e1789ddcf88cfc164368b693f293b3bcf8f
Thành Nguyễn Trí <91453590+thahn0402@users.noreply.github.com>
2023-08-06 20:46:37 +0700

    Update test.java

--- src/main/java/nlu/dacn23backend/entity/test.java
+++ src/main/java/nlu/dacn23backend/entity/test.java
@@ -1,65 +1,105 @@
-package nlu.dacn23backend.entity;
-
-import javax.persistence.*;
-import java.util.Set;
-
-@Entity
-public class User {
-
-    @Id
-    private String userName;
-    private String userFirstName;
-    private String userLastName;
-    private String userPassword;
-    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
-    @JoinTable(name = "USER_ROLE
-    Văn bản vô đây",
-            joinColumns = {
-                    @JoinColumn(name = "USER_ID")
-            },
-            inverseJoinColumns = {
-                    @JoinColumn(name = "ROLE_ID")
-            }
-    )
-    private Set<Role> role;
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    public String getUserFirstName() {
-        return userFirstName;
-    }
-
-    public void setUserFirstName(String userFirstName) {
-        this.userFirstName = userFirstName;
-    }
-
-    public String getUserLastName() {
-        return userLastName;
-    }
-
-    public void setUserLastName(String userLastName) {
-        this.userLastName = userLastName;
-    }
-
-    public String getUserPassword() {
-        return userPassword;
-    }
-
-    public void setUserPassword(String userPassword) {
-        this.userPassword = userPassword;
-    }
-
-    public Set<Role> getRole() {
-        return role;
-    }
-
+//package nlu.dacn23backend.entity;
+//
+//import javax.persistence.*;
+//import java.util.Set;
+//
+//@Entity
+//public class User {
+//
+//    @Id
+//    private String userName;
+//    private String userFirstName;
+//    private String userLastName;
+//    private String userPassword;
+//    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+//    @JoinTable(name = "USER_ROLE
+//    Văn bản vô đây",
+//            joinColumns = {
+//                    @JoinColumn(name = "USER_ID")
+//            },
+//            inverseJoinColumns = {
+//                    @JoinColumn(name = "ROLE_ID")
+//            }
+//    )
+//    private Set<Role> role;
+//
+//    public String getUserName() {
+//        return userName;
+//    }
+//
+//    public void setUserName(String userName) {
+//        this.userName = userName;
+//    }
+//
+//    public String getUserFirstName() {
+//        return userFirstName;
+//    }
+//
+//    public void setUserFirstName(String userFirstName) {
+//        this.userFirstName = userFirstName;
+//    }
+//
+//    public String getUserLastName() {
+//        return userLastName;
+//    }
+//
+//    public void setUserLastName(String userLastName) {
+//        this.userLastName = userLastName;
+//    }
+//
+//    public String getUserPassword() {
+//        return userPassword;
+//    }
+//
+//    public void setUserPassword(String userPassword) {
+//        this.userPassword = userPassword;
+//    }
+//
+//    public Set<Role> getRole() {
+//        return role;
+//    }
+//
+//    public void setRole(Set<Role> role) {
+//        this.role = role;
+//    }
+//}
+class test{
     public void setRole(Set<Role> role) {
-        this.role = role;
-    }
+       this.role = role;
+       String s = "roHiện menu
+doc truyen
+TruyệnThần Đạo Đan TônChương 1870
+THẦN ĐẠO ĐAN TÔN
+Chương 1870: Quy tắc nghiền áp
+
+Lăng Hàn nhìn Đinh Hành, tuy rằng hắn không biết tên của đối phương, nhưng cũng đã nhìn thấy vị này trong Tổ miếu Đinh gia. Nghĩ tới Tổ miếu Đinh gia, Lăng Hàn không khỏi hỏi một câu
+
+- này, Tổ miếu các ngươi đã tu sửa xong chưa vậy?
+
+Con mẹ nó.
+
+Cho dù tu dưỡng của Đinh Hành có thượng thừa tới đâu, có thể tu luyện tới cấp bậc Trảm Trần, ai mà không trải qua vô số mưa gió, tuyejt đối là hỉ nộ ái ố không lộ ra. Thế nhưng Lăng Hàn vừa nói những lời này, Đinh Hành đã lập tức muốn giơ chân lên.
+
+Ngươi còn không biết xấu hổ hỏi sao?
+
+- Ác tử, nếu như không phải ngươi, Tổ miếu Đinh gia ta há có thể bị hủy đi?
+
+Hắn không khỏi rống to nói một câu, ngay cả con mắt cũng xanh.
+
+- Đều một bó to tuổi rồi, còn không biết tu thân dưỡng tính. Rống to, la hét như vậy, làm các tiểu bằng hữu khác sợ thì sao?
+
+Lăng Hàn cười nói, hắn không ngại làm cho người Đinh gia đã rét vì tuyết lại còn lạnh vì sương, sát muối vào miệng vết thương.
+
+- Người nhà ngươi làm ác, còn làm linh vị cho những ác nhân kia làm gì vậy? Đúng là trên sao dưới vậy.
+
+- Chết.
+
+Đinh Hành vốn còn muốn bày tỏ cảm giác ưu việt, khiến cho Lăng Hàn cảm nhận được áp lực tử vong. Cũng không ngờ mới nói hai câu đã bị Lăng Hàn làm cho tức giận muốn nổ tung. Hắn nhịn không được lập tức ra tay về phía Lăng Hàn.
+
+Lăng Hàn còn chưa có ra tay, Nữ Hoàng đã động thủ trước, nàng vừa mới đột phá Trảm Trần, đang kích động không thôi.
+
+Xoát, một đạo kiếm quang xẹt qua, kinh động cửu thiên.
+ License";
+   }
+   
 }
